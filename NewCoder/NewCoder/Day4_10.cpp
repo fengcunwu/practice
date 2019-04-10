421540c50156ea7f6f9e5f7f785944531805f224
fengcunwu <13679146650@163.com>
2019-04-10 14:57:41 +0800

    NewCoder

---
+++ NewCoder/NewCoder/Day4_10.cpp
@@ -0,0 +1,26 @@
+#include "PracticeEveryDay.hpp"
+
+vector<long long> nums(80, 0);
+//牛客网-----客似云来
+// 1 1 2 3 5 8 13 
+// 计算第i天到第j天的和
+void Test_Day4_10_1()
+{
+	nums[0] = 1;
+	nums[1] = 1;
+	for (int i = 2; i < 80; i++)
+	{
+		nums[i] = nums[i - 1] + nums[i - 2];
+	}
+	int from = 0, to = 0;
+	while (cin >> from >> to)
+	{
+		long sum = 0;
+		while (from <= to)
+		{
+			sum += nums[from - 1];
+			from++;
+		}
+		cout << sum << endl;
+	}
+}
